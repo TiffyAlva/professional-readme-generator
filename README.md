23912e3998c87c39ca7721e19f7a70ec81e43f76
TiffyAlva <TiffanyAlvarez54@gmail.com>
2022-09-01 18:28:11 -0400

    added questions

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Professional README Generator Starter Code
+# README-Generator
+
+## User Story & Acceptance Criteria:
+![](./images/user%20story-acceptance%20criteria%20M9.jpg)
+
+## Visuals:
--- index.js
+++ index.js
@@ -1,7 +1,100 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?",
+        //if , else, console.log "Enter your project name."
+        
+    }
+
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project in a short sentences.",
+        //if, else, console.log "Enter installation intructions for your application"
+
+    }
+
+    {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation instructions.",
+        //if, else, console.log "Enter installation for your application"
+
+        
+    }
+
+    
+
+    {
+        type: "list",
+        name: "license",
+        message: "What license are using for your project?"
+        //choices:
+    }
+
+    {
+        type: "input",
+        name: "tests",
+        message: "Provide test instructions for application."
+        //if, else, console.log "Enter instuctions."
+    }
+
+    {
+        type: "input",
+        name: "contribution",
+        message: "Explain how another developer can contribute to the project."
+        //if, else, console.log "Describe contribution to your project."
+    }
+
+    {
+        type: "input",
+        name: "github",
+        message: "Provide your GitHub username."
+        //if, else, console.log "Enter your GitHub username."
+    }
+
+    {
+      type: "input",
+      name: "repo",
+      message: "What is the URL of the GitHub repository?"
+      //if, else, console.log "Enter your GitHub repository."
+
+    }
+
+    {
+        type: "input",
+        name: "author",
+        message: "What is your full name?"
+
+        
+
+    }
+
+    {
+        type: "input",
+        name: "email",
+        message: What is your email?
+        //if, else, console.log "Enter your contact email."
+    }
+
+    {
+        type: "input",
+        name: "userName"
+        message: "What is your GitHub username?"
+    }
+
+
+   
+
+      
+        
+        
+        
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
